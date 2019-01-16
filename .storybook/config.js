105e9a12ca743505c9e4d39889d4f43ae336a162
Rihdus <rihdus.io@gmail.com>
2019-01-17 00:03:05 +0800

    Add storybook, prettier

---
+++ .storybook/config.js
@@ -0,0 +1,18 @@
+import { configure, addDecorator } from '@storybook/react';
+import { withOptions } from '@storybook/addon-options';
+import { themes } from '@storybook/components';
+
+// automatically import all files ending in *.stories.js
+const req = require.context('../packages', true, /.stories.js$/);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+addDecorator(
+  withOptions({
+    name: 'React Primitives',
+    theme: themes.normal,
+  })
+);
+
+configure(loadStories, module);
