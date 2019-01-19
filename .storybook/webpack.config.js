c8c7a2b8fc57470b4f065da529848053ebb4ffc5
Rihdus <rihdus.io@gmail.com>
2019-01-19 19:52:56 +0800

    reset custom webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,9 +1,5 @@
 const path = require("path");
 
 module.exports = (baseConfig, env, defaultConfig) => {
-  // Extend defaultConfig as you need.
-  
-  // defaultConfig.output.publicPath = 'stories';
-
   return defaultConfig;
 };
